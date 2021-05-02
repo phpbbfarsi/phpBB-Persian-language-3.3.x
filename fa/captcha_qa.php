f347098c6b11147fe3312c323b7c368a14d7dc25
Meisam Nobari <meis2m@yahoo.com>
2021-05-02 14:38:39 +0430

    Update captcha_qa.php

--- fa/captcha_qa.php
+++ fa/captcha_qa.php
@@ -8,8 +8,7 @@
 *
 * @copyright (c) phpBB Limited <https://www.phpbb.com>
 * @license GNU General Public License, version 2 (GPL-2.0)
-** Version: 3.3.4 - 1400/02/12
-
+*
 * For full copyright and license information, please see
 * the docs/CREDITS.txt file.
 *
